f99e0c26d397700dafc469a25f3a369cf7f8953d
arespectra <43042472+arespectra@users.noreply.github.com>
2018-09-14 04:01:15 +0300

    Updated kick command

--- commands/kick.js
+++ commands/kick.js
@@ -1,27 +1,43 @@
 const Discord = require("discord.js");
+// const errors = require("../utils/errors.js");
+const getResponse = require('./getResponse');
 
 module.exports.run = async (bot, message, args) => {
-  let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-  if(!kUser) message.channel.send("Can't find user!");
-  let kReason = args.slice(1).join(" ") || "None"
+
+  let mentionedMember = message.mentions.members.first() || message.guild.member(args[0]);
+  if(!mentionedMember || mentionedMember == null) return message.channel.send("Please mention a user or provide their user id!");
+  let kickReason = args.slice(1).join(" ") != '' ? args.slice(1).join(" ") : 'No reason provided';
   if(!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send("Aww sorry but you are not cool enough to do this");
-  if(kUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("That person can't be kicked bro!")
+  if(mentionedMember.hasPermission("MANAGE_MESSAGES") || !mentionedMember.kickable) return message.channel.send("That person can't be kicked bro!");
 
   let kickEmbed = new Discord.RichEmbed()
   .setDescription("~Kick~")
   .setColor("#e56b00")
-  .addField("Kicked User", `${kUser} with ID ${kUser.id}`)
+  .addField("Kicked User", `${mentionedMember} with ID ${mentionedMember.id}`)
   .addField("Kicked By", `<@${message.author.id}> with ID ${message.author.id}`)
   .addField("Kicked In", message.channel)
-  .addField("Reason", kReason);
+  .addField("Reason", kickReason);
 
-  message.guild.member(kUser).kick(kReason);
+  let responseMsg = `\:exclamation: | You are kicking **${mentionedMember.user.tag}** from **${message.guild.name}**\n\`\`\`Kick Reason:\n\n${kickReason}\`\`\`\n \:arrow_right: Please type \`confirm\` or type \`cancel\` `;
+  
+  let sentMessage = await message.channel.send(responseMsg);
 
-  message.channel.send(kickEmbed);
-
-
+  let userResponse = await getResponse(message.channel, message.author, ['confirm', 'cancel'], `\:no: | That is an invalid response. Please try again.`).catch(console.log);
+  console.log(userResponse);
+  sentMessage.delete();
+  if (!userResponse | userResponse == 'cancel') {
+      message.channel.send(`\:information_source: | **${mentionedMember.user.tag}** was not kicked!`)
+  } else {
+      mentionedMember.kick(kickReason).then( () => {
+        message.channel.send(kickEmbed);
+      }).catch(e => {
+          message.channel.send(`**Warning** | Failed to kick **${mentionedMember.user.tag}**`)
+          throw new Error(e);
+      })
+  }
 }
 
-  module.exports.help = {
-    name: "kick"
-  }
+exports.help = {
+  name:"kick"
+}
+
