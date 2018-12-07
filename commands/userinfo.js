97605e79444af514caa79fe27b33699253f594a9
arespectra <43042472+arespectra@users.noreply.github.com>
2018-12-08 01:53:24 +0300

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -3,18 +3,20 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
 let user = message.mentions.members.first() || message.guild.member(args[0]);
+  
+  
 
   var userinfo = new Discord.RichEmbed()
-                .setAuthor(`${user.username}`)
-                .setThumbnail(user.avatarURL)
-                .setDescription("This is the user's info!")
-                .setColor("#bc0000")
-                .addField("Full Username:", `${user.username}#${user.discriminator}`)
-                .addField("ID:", user.id)
-                .addField("Created At:", user.createdAt)
+  .setAuthor(`${user.username}`)
+  .setThumbnail(user.avatarURL)
+  .setDescription("This is the user's info!")
+  .setColor("#bc0000")
+  .addField("Full Username:", `${user.username}#${user.discriminator}`)
+  .addField("ID:", user.id)
+  .addField("Created At:", user.createdAt)
 
-                message.channel.send(userinfo);
-          };
+   message.channel.send(userinfo);
+};
 
 
 
