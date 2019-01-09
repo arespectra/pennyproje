0a4e46832211b2e9ccc8bd9d461356a5f5789660
Ray Perry <rperry@runoranj.com>
2019-01-08 19:48:43 -0600

    Adhoc | Destructuring bites again.

--- commands/ban.js
+++ commands/ban.js
@@ -8,8 +8,6 @@ module.exports.run = async (bot, message, args) => {
         return;
     }
 
-    const { mentions, guild, member, channel, author, createdAt } = message;
-    
     const NO_USER_PROVIDED = `Please mention a user or provide the user's id!`;
     const NO_REASON_PROVIDED = `No reason provided.`;
     const NO_BAN_PERMISSIONS = `Aww, sorry! You're not cool enough to do that!`;
@@ -17,36 +15,36 @@ module.exports.run = async (bot, message, args) => {
     const INVALID_CONFIRMATION = `\:no: | That is an invalid response. Please try again.`
     const FAILED_DM = `\:exclamation: | Failed to DM user after banning!`;
 
-    const mentionedMember = mentions.members.first() || guild.member(args[0]);
+    const mentionedMember = message.mentions.members.first() || message.guild.member(args[0]);
     if (!mentionedMember)
-        return channel.send(NO_USER_PROVIDED);
+        return message.channel.send(NO_USER_PROVIDED);
 
     let banReason = args.slice(1).join(" ").trim();
     if (!banReason.length) banReason = NO_REASON_PROVIDED;
 
-    if (!member.hasPermission("BAN_MEMBERS"))
-        return channel.send(NO_BAN_PERMISSIONS);
+    if (!message.member.hasPermission("BAN_MEMBERS"))
+        return message.channel.send(NO_BAN_PERMISSIONS);
     if (mentionedMember.hasPermission("MANAGE_MESSAGES") || !mentionedMember.bannable)
-        return channel.send(UNBANNABLE_PERSON);
+        return message.channel.send(UNBANNABLE_PERSON);
 
     const banEmbed = new Discord.RichEmbed()
           .setDescription("~Ban~")
           .setColor("#bc0000")
           .addField("Banned User", `${mentionedMember} with ID ${mentionedMember.id}`)
-          .addField("Banned By", `<@${author.id}> with ID ${author.id}`)
-          .addField("Banned In", channel)
-          .addField("Time", createdAt)
+          .addField("Banned By", `<@${message.author.id}> with ID ${message.author.id}`)
+          .addField("Banned In", message.channel)
+          .addField("Time", message.createdAt)
           .addField("Reason", banReason);
 
-    const confirmationReceipt = `\:exclamation: | You are banning **${mentionedMember.user.tag}** from **${guild.name}**\n\`\`\`Ban Reason:\n\n${banReason}\`\`\`\n \:arrow_right: Please type \`confirm\` or type \`cancel\` `;
-    const sentMessage = await channel.send(confirmationReceipt);
-    const userResponse = await getResponse(channel, author, ['confirm', 'cancel'], INVALID_CONFIRMATION).catch(console.log);
+    const confirmationReceipt = `\:exclamation: | You are banning **${mentionedMember.user.tag}** from **${message.guild.name}**\n\`\`\`Ban Reason:\n\n${banReason}\`\`\`\n \:arrow_right: Please type \`confirm\` or type \`cancel\` `;
+    const sentMessage = await message.channel.send(confirmationReceipt);
+    const userResponse = await getResponse(message.channel, message.author, ['confirm', 'cancel'], INVALID_CONFIRMATION).catch(console.log);
     
     console.log(userResponse);
     sentMessage.delete();
     
     if (!userResponse || userResponse === 'cancel') {
-        channel.send(`\:information_source: **${mentionedMember.user.tag}** was not banned!`)
+        message.channel.send(`\:information_source: **${mentionedMember.user.tag}** was not banned!`)
     } else {
         mentionedMember.user
             .send(`\:exclamation: You were banned from our server. \n\n\`\`\`Reason:\n\n${banReason}\`\`\`\n\n*This message is an automated notification.*`)
@@ -54,14 +52,14 @@ module.exports.run = async (bot, message, args) => {
                 mentionedMember
                     .ban(banReason)
                     .then(_ => {
-                        channel.send(banEmbed);
-                        channel.send(`\:exclamation: User **${mentionedMember.user.tag}** was successfully banned from ${guild.name}!`);
+                        message.channel.send(banEmbed);
+                        message.channel.send(`\:exclamation: User **${mentionedMember.user.tag}** was successfully banned from ${message.guild.name}!`);
                     }, banError => {
-                        channel.send(`\:warning: Failed to ban **${mentionedMember.user.tag}**!`)
+                        message.channel.send(`\:warning: Failed to ban **${mentionedMember.user.tag}**!`)
                         throw new Error(banError);
                     });
             }, dmError => {
-                channel.send(FAILED_DM);
+                message.channel.send(FAILED_DM);
                 throw new Error(dmError);
             });
     }
--- commands/kick.js
+++ commands/kick.js
@@ -8,8 +8,6 @@ module.exports.run = async (bot, message, args) => {
         return;
     }
 
-    const { mentions, guild, member, channel, author, createdAt } = message;
-
     const NO_USER_PROVIDED = `Please mention a user or provide the user's id!`;
     const NO_REASON_PROVIDED = `No reason provided.`;
     const NO_KICK_PERMISSIONS = `Aww, sorry! You're not cool enough to do that!`;
@@ -17,36 +15,36 @@ module.exports.run = async (bot, message, args) => {
     const INVALID_CONFIRMATION = `\:no: | That is an invalid response. Please try again.`
     const FAILED_DM = `\:exclamation: | Failed to DM user after kicking!`;
 
-    const mentionedMember = mentions.members.first() || guild.member(args[0]);
+    const mentionedMember = message.mentions.members.first() || message.guild.member(args[0]);
     if (!mentionedMember)
-        return channel.send(NO_USER_PROVIDED);
+        return message.channel.send(NO_USER_PROVIDED);
 
     let kickReason = args.slice(1).join(" ").trim();
     if (!kickReason.length) kickReason = NO_REASON_PROVIDED;
 
-    if (!member.hasPermission("KICK_MEMBERS"))
-        return channel.send(NO_KICK_PERMISSIONS);
+    if (!message.member.hasPermission("KICK_MEMBERS"))
+        return message.channel.send(NO_KICK_PERMISSIONS);
     if (mentionedMember.hasPermission("MANAGE_MESSAGES") || !mentionedMember.kickable)
-        return channel.send(UNKICKABLE_PERSON);
+        return message.channel.send(UNKICKABLE_PERSON);
     
     const kickEmbed = new Discord.RichEmbed()
           .setDescription("~Kick~")
           .setColor("#e56b00")
           .addField("Kicked User", `${mentionedMember} with ID ${mentionedMember.id}`)
-          .addField("Kicked By", `<@${author.id}> with ID ${author.id}`)
-          .addField("Kicked In", channel)
-          .addField("Time", createdAt)
+          .addField("Kicked By", `<@${message.author.id}> with ID ${message.author.id}`)
+          .addField("Kicked In", message.channel)
+          .addField("Time", message.createdAt)
           .addField("Reason", kickReason);
 
     let confirmationReceipt = `\:exclamation: | You are kicking **${mentionedMember.user.tag}** from **${message.guild.name}**\n\`\`\`Kick Reason:\n\n${kickReason}\`\`\`\n \:arrow_right: Please type \`confirm\` or type \`cancel\` `;
-    let sentMessage = await channel.send(responseMsg);
-    const userResponse = await getResponse(channel, author, ['confirm', 'cancel'], INVALID_CONFIRMATION).catch(console.log);
+    let sentMessage = await message.channel.send(confirmationReceipt);
+    const userResponse = await getResponse(message.channel, message.author, ['confirm', 'cancel'], INVALID_CONFIRMATION).catch(console.log);
     
     console.log(userResponse);
     sentMessage.delete();
 
     if (!userResponse || userResponse === 'cancel') {
-        channel.send(`\:information_source: **${mentionedMember.user.tag}** was not kicked!`)
+        message.channel.send(`\:information_source: **${mentionedMember.user.tag}** was not kicked!`)
     } else {
         mentionedMember.user
             .send(`\:exclamation: You were kicked from our server. \n\n\`\`\`Reason:\n\n${kickReason}\`\`\`\n\n*This message is an automated notification.*`)
@@ -54,10 +52,10 @@ module.exports.run = async (bot, message, args) => {
                 mentionedMember
                     .kick(kickReason)
                     .then(_ => {
-                        channel.send(kickEmbed);
-                        channel.send(`\:exclamation: User **${mentionedMember.user.tag}** was successfully kicked from ${message.guild.name}`);
+                        message.channel.send(kickEmbed);
+                        message.channel.send(`\:exclamation: User **${mentionedMember.user.tag}** was successfully kicked from ${message.guild.name}!`);
                     }, kickError => {
-                        message.channel.send(`**Warning** Failed to kick **${mentionedMember.user.tag}**`)
+                        message.channel.send(`\:warning: Failed to kick **${mentionedMember.user.tag}**!`)
                         throw new Error(kickError);
                     })                
             }, dmError => {
