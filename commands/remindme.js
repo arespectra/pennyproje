de5b4218c9d0cb861c98ec4888f78b716a5528be
Ray Perry <rperry@runoranj.com>
2019-01-09 08:55:13 -0600

    Adhoc | Make the reminders ping you.

--- commands/remindme.js
+++ commands/remindme.js
@@ -2,7 +2,8 @@ const Discord = require("discord.js");
 const ms = require("ms");
 
 module.exports.run = async (bot, message, args) => {
-    const NO_TIME_SPECIFIED = `**Specify a time for me to remind you! Usage: \`>remind 15min | Code**\``
+    const INVALID_SYNTAX = `**Specify a time for me to remind you! Usage: \`>remind 15min | Code**\``
+    const NO_USER_PROVIDED = `Sorry! That user isn't part of the server anymore!`;
     const NO_OPERABLE_TIME = `Sorry! You have to specify the time before the text!`;
     const DEFAULT_REMINDER_TEXT = `Here's your reminder!`;
     
@@ -10,14 +11,17 @@ module.exports.run = async (bot, message, args) => {
           .join(" ")
           .split("|")
           .map(stringFragment => stringFragment.trim());
+
+    const memberToPing = message.author;
+    if (!memberToPing) return message.channel.send(NO_USER_PROVIDED);
+    if (fullReminder[0].length === 0) return message.channel.send(INVALID_SYNTAX);
     
     const reminderTime = fullReminder[0];
+    const reminderText = fullReminder[1] || DEFAULT_REMINDER_TEXT;
     const convertedReminderTime = ms(reminderTime);
     
-    if (!reminderTime.length) return message.channel.send(NO_TIME_SPECIFIED);
-    if (!/\d+/.test || !convertedReminderTime) return message.channel.send(NO_OPERABLE_TIME);
+    if (!/\d+/.test(reminderTime) || !convertedReminderTime) return message.channel.send(NO_OPERABLE_TIME);
     
-    const reminderText = fullReminder[1] || DEFAULT_REMINDER_TEXT;
     let remindEmbed = new Discord.RichEmbed()
         .setColor("#15f153")
         .addField("Reminder", `${reminderText}`)
@@ -26,7 +30,7 @@ module.exports.run = async (bot, message, args) => {
     message.channel.send(remindEmbed);
 
     setTimeout(_ => {
-        return message.channel.send(`Hey! You wanted me to remind you: ${reminderText}`)
+        return message.channel.send(`<@${memberToPing.id}> Hey! You wanted me to remind you: ${reminderText}`)
     }, convertedReminderTime);
 };
 
